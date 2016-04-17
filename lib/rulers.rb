258439cf6ed300c4f10df8622ba7ded21e255446
ke-pan <ke_pan@yeah.net>
2016-04-17 22:27:10 +0800

    chapter 1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,11 @@
 require "rulers/version"
+require "rulers/array"
 
 module Rulers
   # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ['Hello from P.K.']]
+    end
+  end
 end
